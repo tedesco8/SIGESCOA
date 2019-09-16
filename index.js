5d5442b1f72a29141e9fa874507aaae202a84c95
Pablo Martin Tedesco Rodriguez <pmtr@localhost.localdomain>
2019-09-16 16:31:04 -0300

    organizacion de la arquitectura e instalacion de dependencias basicas

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+
+//muestra peticiones http en la consola
+app.use(morgan('dev'));
+
+//gestiono peticiones remotas
+app.use(cors());
+
+//permito que el servidor reciba peticiones JSON a traves de post
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//asigno puerto por defecto del sistema, o en su defecto el puerto 3000
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), () => {
+    console.log('server on port ' + app.get('port'));
+});
