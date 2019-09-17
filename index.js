bc8a9092f98b2e5a144c25ee8483e816dad10808
Pablo Martin Tedesco Rodriguez <pmtr@localhost.localdomain>
2019-09-17 01:37:10 -0300

    variables de entorno

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ import cors from 'cors';
 import path from 'path';
 import mongoose from 'mongoose';
 import router from './routes/';
+import {} from 'dotenv/config';
 
 mongoose.Promise = global.Promise;
-const dbUrl = 'mongodb+srv://root:xxx8090235pp@cluster0-kzisw.gcp.mongodb.net/test?retryWrites=true&w=majority'
-mongoose.connect(dbUrl, {useCreateIndex:true, useNewUrlParser: true})
+const dbUrl = `mongodb+srv://${process.env.USR_DB}:${process.env.PSW_DB}@cluster0-kzisw.gcp.mongodb.net/test?retryWrites=true&w=majority`
+mongoose.connect(dbUrl, {useCreateIndex:true, useNewUrlParser: true, useUnifiedTopology: true})
 .then(mongoose => console.log('Conectado a la base de datos'))
 .catch(err => console.log(err));
 
-const app = express();
+const app = express(); 
 
 //muestra peticiones http en la consola
 app.use(morgan('dev'));
@@ -29,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', router);
 
 //asigno puerto por defecto del sistema, o en su defecto el puerto 3000
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || process.env.PORT_DEF);
 
 app.listen(app.get('port'), () => {
     console.log('server on port ' + app.get('port'));
