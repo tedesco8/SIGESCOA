18f60d4f23aba5c8d414fea49e392ddd6847f53d
Pablo Tedesco <tedesco8@hotmail.com>
2019-09-23 17:04:18 -0300

    restringiendo las rutas de categorias y articulos

--- routes/articulo.js
+++ routes/articulo.js
@@ -1,14 +1,15 @@
 import routerx from 'express-promise-router';
 import articuloController from '../controllers/ArticuloControllers';
+import auth from '../middlewares/auth';
 
 const router = routerx();
 
-router.post('/add', articuloController.add);
-router.get('/query', articuloController.query);
-router.get('/list', articuloController.list);
-router.put('/update', articuloController.update);
-router.delete('/remove', articuloController.remove);
-router.put('/activate', articuloController.activate);
-router.put('/deactivate', articuloController.deactivate);
+router.post('/add', auth.verifyAlmacenero,articuloController.add);
+router.get('/query', auth.verifyAlmacenero,articuloController.query);
+router.get('/list', auth.verifyAlmacenero,articuloController.list);
+router.put('/update', auth.verifyAlmacenero,articuloController.update);
+router.delete('/remove', auth.verifyAlmacenero,articuloController.remove);
+router.put('/activate', auth.verifyAlmacenero,articuloController.activate);
+router.put('/deactivate', auth.verifyAlmacenero,articuloController.deactivate);
 
 export default router;
--- routes/categoria.js
+++ routes/categoria.js
@@ -1,14 +1,15 @@
 import routerx from 'express-promise-router';
 import categoriaController from '../controllers/CategoriaController';
+import auth from '../middlewares/auth';
 
 const router = routerx();
 
-router.post('/add', categoriaController.add);
-router.get('/query', categoriaController.query);
-router.get('/list', categoriaController.list);
-router.put('/update', categoriaController.update);
-router.delete('/remove', categoriaController.remove);
-router.put('/activate', categoriaController.activate);
-router.put('/deactivate', categoriaController.deactivate);
+router.post('/add', auth.verifyAlmacenero,categoriaController.add);
+router.get('/query', auth.verifyAlmacenero,categoriaController.query);
+router.get('/list', auth.verifyAlmacenero,categoriaController.list);
+router.put('/update', auth.verifyAlmacenero,categoriaController.update);
+router.delete('/remove', auth.verifyAlmacenero,categoriaController.remove);
+router.put('/activate', auth.verifyAlmacenero,categoriaController.activate);
+router.put('/deactivate', auth.verifyAlmacenero,categoriaController.deactivate);
 
 export default router;
