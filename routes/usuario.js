b3f62954a95941e622f03f7055837abdc808799a
Pablo Tedesco <tedesco8@hotmail.com>
2019-09-23 17:00:25 -0300

    middlewares, acceso a las rutas segun rol de usuario

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,15 +1,16 @@
 import routerx from 'express-promise-router';
 import usuarioController from '../controllers/UsuarioController';
+import auth from '../middlewares/auth';
 
 const router = routerx();
 
-router.post('/add', usuarioController.add);
-router.get('/query', usuarioController.query);
-router.get('/list', usuarioController.list);
-router.put('/update', usuarioController.update);
-router.delete('/remove', usuarioController.remove);
-router.put('/activate', usuarioController.activate);
-router.put('/deactivate', usuarioController.deactivate);
+router.post('/add', auth.verifyAdministrador,usuarioController.add);
+router.get('/query', auth.verifyAdministrador,usuarioController.query);
+router.get('/list', auth.verifyAdministrador,usuarioController.list);
+router.put('/update', auth.verifyAdministrador,usuarioController.update);
+router.delete('/remove', auth.verifyAdministrador,usuarioController.remove);
+router.put('/activate', auth.verifyAdministrador,usuarioController.activate);
+router.put('/deactivate', auth.verifyAdministrador,usuarioController.deactivate);
 router.post('/login', usuarioController.login);
 
 export default router;
