23b691a24545276d0bb82c96ff85ec1de567356a
Pablo Tedesco <tedesco8@hotmail.com>
2021-01-17 21:15:31 -0300

    mas permisos a invitado

--- routes/venta.js
+++ routes/venta.js
@@ -5,8 +5,8 @@ import auth from '../middlewares/auth';
 const router=routerx();
 
 router.post('/add',auth.verifyUsuario,ventaController.add);
-router.get('/query',auth.verifyVendedor,ventaController.query);
-router.get('/list',auth.verifyVendedor,ventaController.list);
+router.get('/query',auth.verifyUsuario,ventaController.query);
+router.get('/list',auth.verifyUsuario,ventaController.list);
 router.get('/grafico12meses',auth.verifyUsuario,ventaController.grafico12Meses);
 router.get('/consultaFechas',auth.verifyUsuario,ventaController.consultaFechas);
 router.put('/update',auth.verifyUsuario,ventaController.update);
